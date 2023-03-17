705d90178ae2a261a2c7642a839da7b517462915
Ahmed Shaban <ahmed.shaaban.elhdah@gmail.com>
2023-03-17 11:39:32 +0200

    get post screen
    - add post screen
    - add mock data
    - make design for post
    - use props for send data

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { StyleSheet, SafeAreaView, StatusBar } from 'react-native';
+import PostScreen from './screen/PostScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <PostScreen></PostScreen>
+    
+    </SafeAreaView>
   );
 }
 
@@ -14,7 +15,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop:StatusBar.currentHeight
   },
 });
+
+
+
+
